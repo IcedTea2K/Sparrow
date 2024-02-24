c1ce718a4285845782952eef00bb772c8aabba0e
Minh Au <auduongminh2004@gmail.com>
2024-02-23 19:41:10 -0500

    add color extension to accept hex values

--- Views/RoundTableView.swift
+++ Views/RoundTableView.swift
@@ -14,7 +14,7 @@ struct RoundTableView: View {
             Table()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color("#e9edc9"))
+        .background(Color(hex: "#e9edc9"))
     }
 }
 
@@ -24,19 +24,20 @@ struct Table: View {
     var body: some View {
         let diameter: CGFloat = 250
         let radius: CGFloat = diameter / 2
-        let space = CGFloat(2 * Double.pi / Double(people.count))
+        let space = 2 * CGFloat.pi / CGFloat(people.count)
+        let randStart = CGFloat.random(in: 0 ... 2 * CGFloat.pi)
         
         ZStack {
             Circle()
-                .fill(Color("#ccd5ae"))
+                .fill(Color(hex: "#ccd5ae"))
                 .frame(width: diameter, height: diameter)
                 .onTapGesture {
                     people.append("Rey")
                 }
             ForEach(Array(people.enumerated()), id: \.offset) { i, person in
                 Person(name: person, size: 50)
-                    .offset(x: (radius + 25) * cos(space * CGFloat(i)),
-                            y: (radius + 25) * sin(space * CGFloat(i)))
+                    .offset(x: (radius + 25) * cos(randStart + space * CGFloat(i)),
+                            y: (radius + 25) * sin(randStart + space * CGFloat(i)))
             }
         }
     }
@@ -48,7 +49,7 @@ struct Person: View {
     var size: CGFloat // circle radius for now
     var body: some View {
         Circle()
-            .fill(Color("#d4a373"))
+            .fill(Color(hex: "#d4a373"))
             .frame(width: size, height: size)
     }
 }
---
+++ Views/utilities.swift
@@ -0,0 +1,25 @@
+//
+//  utilities.swift
+//  Sparrow
+//
+//  Created by Minh Au on 2024-02-23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: String(hex.dropFirst()))
+        var res: UInt64 = 0
+        
+        scanner.currentIndex = scanner.string.startIndex
+        scanner.scanHexInt64(&res)
+        
+        let red = Double((res & 0xFF0000) >> 16) / 255.0
+        let green = Double((res & 0x00FF00) >> 8) / 255.0
+        let blue = Double(res & 0x0000FF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue)
+    }
+}
