1fd27598e609a46fb43e20c94f60133af1e86b7b
Minh Au <auduongminh2004@gmail.com>
2024-02-24 12:55:04 -0500

    a working stopWatch!!

--- Views/RoundTableView.swift
+++ Views/RoundTableView.swift
@@ -11,12 +11,13 @@ import Combine
 
 struct RoundTableView: View {
     var body: some View {
-        let stopWatch = StopWatch()
-//        let _ = stopWatch.startClock()
+        let clock = Clock()
         VStack {
-                stopWatch
+                StopWatch()
                     .offset(x: 0, y: -150)
+                    .environmentObject(clock)
             Table()
+                .environmentObject(clock)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(hex: "#e9edc9"))
@@ -26,8 +27,9 @@ struct RoundTableView: View {
 struct Table: View {
     @State
     private var _people: [Person] = []
+    private let randStart = CGFloat.random(in: 0 ... 2 * CGFloat.pi)
     
-    private let colors: [String] = [
+    private let _colors: [String] = [
         "#D1C6DF", "#B2A8D5", "#FADBC7", "#475164", "#FFA7A7",
         "#F8E5B3", "#9AD4C5", "#EEA9A2", "#A5ADBB", "#EBEFF2",
         "#F4C29E", "#BFB4C4", "#FFB08E", "#6AB1E3", "#F9CF8D",
@@ -36,19 +38,26 @@ struct Table: View {
         "#D7C9E5", "#FFAB90", "#B3E1C2", "#E5D6ED", "#FAD9A9"
     ]
     
+    @EnvironmentObject
+    private var _clock: Clock
+    
     var body: some View {
         let diameter: CGFloat = 250
         let radius: CGFloat = diameter / 2
         let space = 2 * CGFloat.pi / CGFloat(_people.count)
-        let randStart = CGFloat.random(in: 0 ... 2 * CGFloat.pi)
         
         ZStack {
             Circle()
                 .fill(Color(hex: "#ccd5ae"))
                 .frame(width: diameter, height: diameter)
                 .onTapGesture {
-                    let newPerson = Person(name: "rey", size: 30, color: colors.randomElement())
+                    let newPerson = Person(name: "rey", size: 30, color: _colors.randomElement())
                     _people.append(newPerson)
+                    if !_clock.isRunng() {
+                        _clock.startClock()
+                    } else {
+                        _clock.stopClock()
+                    }
                 }
             
             ForEach(Array(_people.enumerated()), id: \.offset) { i, person in
@@ -107,51 +116,12 @@ struct Person: View {
 }
 
 struct StopWatch: View {
-    @State private var _currTimeStr: String = "00 : 00"
-    @State private var _currTime: Date = Date.now
-    private var _timer: Publishers.Autoconnect<Timer.TimerPublisher>? = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    private let _dateFormater: DateFormatter = DateFormatter()
-    @State private var _status = false
-    @State private var _startTime: Date = .now
-    
-    init() {
-        _dateFormater.dateFormat = "mm : ss"
-    }
+    @EnvironmentObject var clock: Clock
     
     var body: some View {
-        if _timer != nil {
-            Text(_currTimeStr)
-                .font(.largeTitle)
-                .onReceive(_timer!, perform: { newTime in
-                    _currTime = newTime
-//                    _currTimeStr = _dateFormater.string(from: _currTime)
-                    let duration = Int(_currTime.timeIntervalSince(_startTime))
-                    let sec = duration % 60
-                    let min = duration / 60
-                    _currTimeStr = String(format: "%02d : %02d", min, sec)
-                })
-        } else {
-            Text(_currTimeStr)
-                .font(.largeTitle)
-        }
-            
-    }
-    
-    func startClock() {
-        _status = true
-        _startTime = _currTime
-    }
-    
-    func stopClock() {
-        _status = false
-    }
-    
-    func calculateDuration(time: Date) {
-        
-    }
-    
-    func isRunning() -> Bool {
-        return _status
+        Text(clock.getCurrTime())
+            .font(.largeTitle)
+            .foregroundStyle(.black)
     }
 }
 
--- Views/utilities.swift
+++ Views/utilities.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import Combine
 
 extension Color {
     init(hex: String) {
@@ -28,5 +29,54 @@ struct PersonPreferenceKey: PreferenceKey {
     static var defaultValue: CGPoint = .zero
     static func reduce(value: inout CGPoint, nextValue: () -> CGPoint) {
         value = nextValue()
+    }
+}
+
+class Clock: ObservableObject {
+    @Published private var _currTimeStr: String = "00 : 00"
+    private var _currTime: Date = .now
+    private var _startTime: Date = .now
+    
+    private var _clock = Timer.publish(every: 1, on: .main, in: .common)
+    private var _clockDestroyer: AnyCancellable? = nil
+    private var _status = false
+    
+    public func startClock() {
+        resetTime()
+        
+        _status = true
+        _startTime = .now
+        
+        _clockDestroyer = _clock
+            .autoconnect()
+            .sink(receiveValue: { time in
+                self._currTime = time
+                self._currTimeStr = self._getDurationSinceStart()
+            })
+    }
+    
+    public func stopClock() {
+        _status = false
+        _clockDestroyer?.cancel()
+        _clockDestroyer = nil
+    }
+    
+    public func getCurrTime() -> String{
+        return _currTimeStr
+    }
+    
+    public func isRunng() -> Bool {
+        return _status
+    }
+    
+    private func resetTime() {
+        _currTimeStr = "00 : 00"
+    }
+    
+    private func _getDurationSinceStart() -> String {
+        let duration = Int(_currTime.timeIntervalSince(_startTime))
+        let min      = duration / 60
+        let sec      = duration % 60
+        return String(format: "%02d : %02d", min, sec)
     }
 }
