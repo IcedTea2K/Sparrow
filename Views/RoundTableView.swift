5aac48141fba64532b17515b8da0a9c8fcfd780e
Minh Au <auduongminh2004@gmail.com>
2024-02-23 20:15:01 -0500

    add random colors to people

--- Views/RoundTableView.swift
+++ Views/RoundTableView.swift
@@ -21,6 +21,15 @@ struct RoundTableView: View {
 struct Table: View {
     @State
     private var people: [String] = []
+    
+    private let colors: [String] = [
+        "#D1C6DF", "#B2A8D5", "#FADBC7", "#475164", "#FFA7A7",
+        "#F8E5B3", "#9AD4C5", "#EEA9A2", "#A5ADBB", "#EBEFF2",
+        "#F4C29E", "#BFB4C4", "#FFB08E", "#6AB1E3", "#F9CF8D",
+        "#A8DEC9", "#E7B3A0", "#7E5C8F", "#FAD685", "#B3E1C2",
+        "#D9CCEA", "#F7C67A", "#AEC8EF", "#F9E2D2", "#B9BBC2",
+        "#D7C9E5", "#FFAB90", "#B3E1C2", "#E5D6ED", "#FAD9A9"
+    ]
     var body: some View {
         let diameter: CGFloat = 250
         let radius: CGFloat = diameter / 2
@@ -35,9 +44,9 @@ struct Table: View {
                     people.append("Rey")
                 }
             ForEach(Array(people.enumerated()), id: \.offset) { i, person in
-                Person(name: person, size: 50)
-                    .offset(x: (radius + 25) * cos(randStart + space * CGFloat(i)),
-                            y: (radius + 25) * sin(randStart + space * CGFloat(i)))
+                Person(name: person, size: 30, color: colors.randomElement())
+                    .offset(x: (radius + 30 / 2) * cos(randStart + space * CGFloat(i)),
+                            y: (radius + 30 / 2) * sin(randStart + space * CGFloat(i)))
             }
         }
     }
@@ -47,9 +56,11 @@ struct Table: View {
 struct Person: View {
     var name: String
     var size: CGFloat // circle radius for now
+    @State
+    var color: String? // hex string
     var body: some View {
         Circle()
-            .fill(Color(hex: "#d4a373"))
+            .fill(Color(hex: color!))
             .frame(width: size, height: size)
     }
 }
